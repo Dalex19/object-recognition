75cfb3a2b569e3aadb82cfe7da13a2d2f00ee567
Dalex19 <dearalex875@gmail.com>
2022-10-20 20:50:18 -0600

    refactor: code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-lime-400 flex justify-center items-center">
+    <div className="w-screen h-screen bg-gradient-to-r from-green-500 to-emerald-900 flex justify-center items-center">
       <Routes>
         <Route
           path="/"
-          element={<h1 className="text-5xl">Hi, I am Home</h1>}
+          element={<Home/>}
         />
         <Route
           path="/preview"
